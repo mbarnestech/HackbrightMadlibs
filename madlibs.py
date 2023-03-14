422256b5f64dc09898abfe69dec066fef0f8e53b
mbarnestech <maggie.barnes@gmail.com>
2023-03-14 15:03:03 -0600

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,87 @@
+"""A madlib game that compliments its users."""
+
+from random import choice
+
+from flask import Flask, render_template, request
+
+# "__name__" is a special Python variable for the name of the current module.
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    "awesome",
+    "terrific",
+    "fantastic",
+    "neato",
+    "fantabulous",
+    "wowza",
+    "oh-so-not-meh",
+    "brilliant",
+    "ducky",
+    "coolio",
+    "incredible",
+    "wonderful",
+    "smashing",
+    "lovely",
+]
+
+
+@app.route("/")
+def start_here():
+    """Display homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route("/hello")
+def say_hello():
+    """Say hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route("/greet")
+def greet_person():
+    """Greet user with compliment."""
+
+    player = request.args.get("person")
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html", person=player, compliment=compliment)
+
+@app.route("/game")
+def show_madlib_form():
+
+    answer = request.args.get("game")
+    if answer == "no":
+        return render_template("goodbye.html")
+    elif answer == "yes":
+        return render_template("game.html")
+    else:
+        return render_template("error.html")
+    
+@app.route("/madlib")
+def show_madlib():
+
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+    person = request.args.get("person")
+    color = request.args.get("color")
+
+
+    madlib_dict = {
+        'color': color,
+        'person': person,
+        'adjective': adjective,
+        'noun': noun,
+    }
+    
+
+    return render_template("madlib.html", **madlib_dict)
+
+if __name__ == "__main__":
+    # Setting debug=True gives us error messages in the browser and also
+    # "reloads" our web app if we change the code.
+
+    app.run(debug=True, host="0.0.0.0")
